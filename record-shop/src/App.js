bceeff0af91618f54545bc0b0c6eb65c76c09e04
khushal <totakhailkhushal@gmail.com>
2021-08-25 10:02:53 +0200

    New changes

--- record-shop/src/App.js
+++ record-shop/src/App.js
@@ -1,7 +1,54 @@
+import React, { useState } from 'react'
+import LoginForm from './component/LoginForm';
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World , test</div>;
+  const adminUser = {
+    email: "user@user.com",
+    password: "user123"
+  }
+
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("Logged In ");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    } else {
+      console.log("Details do not match");
+      setError("Details do not match!");
+    }
+  }
+
+  const Logout = () => {
+    setUser({ name: "", email: "" })
+    console.log("Logout");
+  }
+
+
+  return (
+    <div className="App">
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2> Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+    </div>
+
+  );
 }
+
 
 export default App;
