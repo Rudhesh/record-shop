38259ce56626e07d88342c920fb1a34bc20e9249
Rudesh <rudeshdhote@gmail.com>
2024-09-13 21:46:54 +0200

    refactor the frontendcode

--- client/src/component/MainLogin.js
+++ client/src/component/MainLogin.js
@@ -49,7 +49,7 @@ const MainLogin = ({ setLoginUser1 }) => {
       } else {
         console.error("Unexpected data format", res.data);
       }
-      history.push("https://record-shop-one.vercel.app/main");
+      history.push("/main");
     })
     .catch((err) => {
       console.error("Login failed", err);
